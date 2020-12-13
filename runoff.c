1152adfb098f2e21284a5d4b4f56b3046bf51d5d
mgxmajewski <mgxmajewski@gmail.com>
2020-12-13 22:47:38 +0000

    Add print_winner when majority reached

--- runoff.c
+++ runoff.c
@@ -165,7 +165,15 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    // TODO
+    float majority = ceil(voter_count/2);
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes > majority)
+        {
+            printf("%s\n", candidates[i].name);
+            return true;
+        }
+    }
     return false;
 }
 
