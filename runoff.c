db872609762979302685894a1f89b961e2cdcd46
mgxmajewski <mgxmajewski@gmail.com>
2020-12-13 21:33:55 +0000

    Add loop - updating preferences array

--- runoff.c
+++ runoff.c
@@ -128,7 +128,18 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    if ()
+    
+    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        int valid_vote = strcmp(candidates[i].name, name);
+        if (valid_vote == 0)
+        {
+              preferences[voter][rank] = i;
+              return true;
+        }
+    }
+    
     return false;
 }
 
